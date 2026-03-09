05fbb383d936b74234c4586c86fdb696db794c55
GlorryssDEV <dronik1098@gmail.com>
2026-03-09 20:28:56 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+Glorrys = {}
+
+Glorrys.TextColorHP = {r=255,g=0,b=0}
+Glorrys.TextColorArmour = {r=0,g=0,b=255}
+Glorrys.TextColorHeadshot = {r=255,g=140,b=0}
+Glorrys.TextSize = 15
+Glorrys.HeadshotSize = 18
+Glorrys.Duration = 5000
