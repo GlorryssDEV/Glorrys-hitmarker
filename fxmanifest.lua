6ecdd6bd204279ee41de6ab6b41d5ebf3cbb1f61
GlorryssDEV <dronik1098@gmail.com>
2026-03-09 20:28:10 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+Author 'Glorrys'
+Description 'Advanced Hitmarker'
+Version '1.0.0'
+
+ui_page 'ui/ui.html'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+
+files {
+    'ui/ui.html'
+}
